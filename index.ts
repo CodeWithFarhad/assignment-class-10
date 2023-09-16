5940cad0a6ef4a2c3429c4de4bb9d2276ebf3c4c
CodeWithFarhad <codewithfarhad@gmail.com>
2023-09-16 20:15:52 +0500

    data

--- index.ts
+++ index.ts
@@ -453,4 +453,224 @@ if(favorite_fruits.includes("Mangoes")){
   }
 
 
-  ,
+//   Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted
+
+{
+
+
+let currentUsers: string[] = ["Hadi", "Fatima", "Dawood", "Ayesha", "Waleed"];
+
+let newUsers: string[] = ["Hadi", "Qamar", "Ali", "Sadia", "Waleed"];
+
+function isUsernameTaken(username: string): boolean {
+    return currentUsers.some(user => user.toLowerCase() === username.toLowerCase());
+}
+
+for (let newUser of newUsers) {
+    if (isUsernameTaken(newUser)) {
+        console.log(`Sorry, the username '${newUser}' is already taken. Please choose a different one.`);
+    } else {
+        console.log(`Congratulations! The username '${newUser}' is available.`);
+    }
+}
+
+}
+// Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+
+// • Loop through the array.
+
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+{
+  let numbers2: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let number of numbers2) {
+    let ordinal: string;
+
+    if (number === 1) {
+        ordinal = "1st";
+    } else if (number === 2) {
+        ordinal = "2nd";
+    } else if (number === 3) {
+        ordinal = "3rd";
+    } else {
+        ordinal = `${number}th`;
+    }
+
+    console.log(ordinal);
+}
+}
+
+
+
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+
+{
+
+let favPizzas: string[] = ["ExtraCheese", "Fagita", "BBQ"];
+
+console.log("My favorite pizzas:");
+for (let pizza of favPizzas) {
+    console.log(pizza);
+}
+
+console.log("\nMy thoughts on each pizza:");
+for (let pizza of favPizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("\nI really love pizza!");
+
+
+
+}
+
+
+
+// Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+{
+let animals: string[] = ["Parrot", "Cat", "Dog"];
+
+console.log("Common characteristic: They are all mammals.");
+console.log("Names of animals:");
+for (let animal of animals) {
+    console.log(animal);
+}
+
+console.log("Statements about each animal:");
+for (let animal of animals) {
+    console.log(`A ${animal.toLowerCase()} would make a great pet.`);
+}
+
+console.log("Any of these animals will be good pet!");
+}
+
+
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+{
+function makeShirt(size: string, message: string): void {
+    console.log(`You have ordered a ${size}-sized shirt with the message: "${message}".`);
+}
+
+
+makeShirt("Medium", "supreme");  
+
+
+ 
+
+}
+
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+
+{
+  function makeShirts(size: string = "Large", message: string = "I love TypeScript"): void {
+    console.log(`You have ordered a ${size}-sized shirt with the message: "${message}".`);
+}
+
+makeShirts();
+
+makeShirts("Medium");
+
+makeShirts("Small", "Old Skool");
+
+}
+
+
+// Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+{
+function describeCity(city: string, country: string = "Unknown") {
+    console.log(`${city} is in ${country}.`);
+}
+
+describeCity("Faisalabad", "Pakistan");
+describeCity("Sydney", "Australia");
+describeCity("Seoul"); 
+
+}
+
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+// "Lahore, Pakistan"
+
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+
+
+{
+
+function cityCountry(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+const location1 = cityCountry("Lahore", "Pakistan");
+const location2 = cityCountry("Mumbai", "India");
+const location3 = cityCountry("sharjah", "UAE");
+
+console.log(location1);
+console.log(location2);
+console.log(location3);
+
+
+}
+
+
+
+
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+{
+
+function showMagicians1(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+let magicianNames: string[] = ["Harry Potter", "Geralt", "Aib Ken"];
+
+showMagicians1(magicianNames);
+
+}
+
+// Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+{
+
+interface Car {
+    manufacturer: string;
+    modelName: string;
+    [key: string]: any; 
+  }
+  
+  function createCar(manufacturer: string, modelName: string, ...options: [string, any][]): Car {
+    const car: Car = {
+      manufacturer,
+      modelName,
+    };
+  
+    for (const [key, value] of options) {
+      car[key] = value;
+    }
+  
+    return car;
+  }
+  
+  const myCar = createCar("Mercedese", "Honda", ["color", "black"], ["year", 2024]);
+
+
+
+  
+  console.log(myCar);
+  
+}
+
+
